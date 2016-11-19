40c097f06b6ed261e2355ad1d47c1ce2804bf225
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-19 15:42:25 +0100

    Statusbar: correctly align status bar icons
    Change-Id: I871b3ad79142dfcfd8822df6b3808d5ae563b1cc

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -40,7 +40,7 @@ import com.android.systemui.statusbar.policy.BatteryController;
 public class BatteryMeterDrawable extends Drawable implements
         BatteryController.BatteryStateChangeCallback {
 
-    private static final float ASPECT_RATIO = 9.5f / 14.5f;
+    private static final float ASPECT_RATIO = 9.5f / 14.3f;
     public static final String TAG = BatteryMeterDrawable.class.getSimpleName();
     public static final String SHOW_PERCENT_SETTING = "status_bar_show_battery_percent";
 
@@ -458,7 +458,7 @@ public class BatteryMeterDrawable extends Drawable implements
             mTextHeight = -mTextPaint.getFontMetrics().ascent;
             pctText = String.valueOf(SINGLE_DIGIT_PERCENT ? (level/10) : level);
             pctX = mWidth * 0.5f;
-            pctY = (mHeight + mTextHeight) * 0.47f;
+            pctY = (mHeight + mTextHeight) * 0.46f;
             pctOpaque = levelTop > pctY;
             if (!pctOpaque) {
                 mTextPath.reset();
