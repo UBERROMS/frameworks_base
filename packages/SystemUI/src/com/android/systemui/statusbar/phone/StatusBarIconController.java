9a03384923eab9a9f23e5479ae591477130b3804
dooyoung.hwang <dooyoung.hwang@lge.com>
2016-12-22 20:04:20 +0900

    Revert "base: battery style: fix layout changes"
    This reverts commit 7077c6f65fc0922c5e0875556717f5fa2803648e.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -180,6 +180,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
 
         //mBatteryMeterView.setLayoutParams(scaledLayoutParams);
         //mBatteryMeterViewKeyguard.setLayoutParams(scaledLayoutParams);
+        //mBatteryViewManager.setLayoutParams(scaledLayoutParams);
     }
 
     @Override
@@ -632,7 +633,6 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
                     ViewGroup.LayoutParams.WRAP_CONTENT, mIconSize);
             child.setLayoutParams(lp);
         }
-        mBatteryViewManager.onDensityOrFontScaleChanged();
         //scaleBatteryMeterViews(mContext);
     }
 
